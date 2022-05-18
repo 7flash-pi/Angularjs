f0cd5d9a6f5a5c1c55e12d07806bb261952e7c77
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-05-18 14:52:33 +0530

    day2 2nd

--- Day2/success-failure/src/app/app.module.ts
+++ Day2/success-failure/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { warningAlertComponent } from './warningAlert/warningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    warningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
