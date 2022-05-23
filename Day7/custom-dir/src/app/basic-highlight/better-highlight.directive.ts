107ea95564da99ee7f5ffd4fca99210d6b6c4adb
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-05-24 00:44:48 +0530

    day 7 last

--- Day7/custom-dir/src/app/basic-highlight/better-highlight.directive.ts
+++ Day7/custom-dir/src/app/basic-highlight/better-highlight.directive.ts
@@ -1,14 +1,29 @@
-    import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+    import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective  implements OnInit{
 
+    @Input() defaultColor:string='transparent';
+    @Input() highlightColor:string='blue';
+    @HostBinding('style.backgroundColor') backgroundColor;
+
   constructor( private renderer:Renderer2 ,private elRef:ElementRef) { }
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue',);
-    
+    //this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+    this.backgroundColor=this.defaultColor;
+  }
+
+  @HostListener('mouseenter') mouseover(eventData:Event){ 
+   // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+    this.backgroundColor=this.highlightColor;
+
+  }
+  @HostListener('mouseleave') mouseleave(eventData:Event){ 
+    //this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
+     this.backgroundColor=this.defaultColor;
+
   }
 
 }
