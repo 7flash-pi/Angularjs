8aba1d1a1f174cfd9e6fa2190af33128ee420208
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-05-24 10:04:41 +0530

    day8 1st

--- Day4/Recipe-book/src/app/app.module.ts
+++ Day4/Recipe-book/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipieItemComponent } from './recipies/recipie-list/recipie-item/recip
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { FormsModule } from '@angular/forms';
     RecipieDetailComponent,
     RecipieItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
