109c07bac9f419bbfc3063777ecb3c1522c24059
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-06-07 21:53:57 +0530

    final build

--- Projects/weatherApp/src/environments/environment.prod.ts
+++ Projects/weatherApp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+   weatherApiBaseUrl:'https://community-open-weather-map.p.rapidapi.com/weather',
+  
+  XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'community-open-weather-map.p.rapidapi.com',
+
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'5f53c9e797mshf5c48c1e05ac722p1fd60bjsnc8c92a169315',
+
+
 };
