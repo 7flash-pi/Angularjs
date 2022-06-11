e6687bd5ea9f9342a8c22624bb26b43aaf91c36d
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-06-11 12:24:11 +0530

    final

--- Projects/random-text-generator/src/app/app.component.ts
+++ Projects/random-text-generator/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TextService } from './text.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'random-text-generator';
+export class AppComponent implements OnInit{
+  inputNumber:number;
+  Randomtext:string[];
+  constructor(private textService:TextService){}
+
+  ngOnInit(): void {
+    this.Randomtext=this.textService.randomText;
+  }
+  handleSubmit=()=>{
+    let amount=this.inputNumber;
+    if(this.inputNumber<=0){
+      amount=1;
+    }
+    if(this.inputNumber>8)
+    {
+      amount=8;
+    }
+
+        if(Array.isArray(this.Randomtext)){
+    this.Randomtext=this.Randomtext.slice(0,amount);
+
+         }
+        else {
+          console.log('Randomtext is not an array');
+        }
+}
+
 }
